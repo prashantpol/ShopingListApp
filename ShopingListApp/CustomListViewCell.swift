ed5956e489c17a2d404ea9a75fb996766b0ef7bc
prashant pol <prashantn.pol@gmail.com>
2017-02-21 20:04:29 -0500

    List/AddNewListName/Design Added

---
+++ ShopingListApp/CustomListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomListViewCell.swift
+//  ShopingListApp
+//
+//  Created by Prashant Pol on 2017-02-21.
+//  Copyright © 2017 Polorg. All rights reserved.
+//
+
+import UIKit
+
+class CustomListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ShopingListApp/ShoppingListModel.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingListModel.swift
+//  ShopingListApp
+//
+//  Created by Prashant Pol on 2017-02-21.
+//  Copyright © 2017 Polorg. All rights reserved.
+//
+
+import Foundation
